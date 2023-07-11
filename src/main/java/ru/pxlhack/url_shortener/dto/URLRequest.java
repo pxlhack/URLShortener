8ab66752197307d8e6dd83b0581f5852d90dd93e
pxlhack <nellsouhd@gmail.com>
2023-07-11 19:15:12 +0700

    feat: Add validation for URLRequest

--- src/main/java/ru/pxlhack/url_shortener/dto/URLRequest.java
+++ src/main/java/ru/pxlhack/url_shortener/dto/URLRequest.java
@@ -1,4 +1,13 @@
 package ru.pxlhack.url_shortener.dto;
 
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class URLRequest {
+    @NotEmpty
+    private String url;
 }
