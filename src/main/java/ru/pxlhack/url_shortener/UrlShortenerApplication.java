f7bfff5c8965f1be007322dedcf975d3cbcb8b74
pxlhack <nellsouhd@gmail.com>
2023-07-11 18:39:48 +0700

    feat: Add Dockerfile, docker-compose.yaml.

--- src/main/java/ru/pxlhack/url_shortener/UrlShortenerApplication.java
+++ src/main/java/ru/pxlhack/url_shortener/UrlShortenerApplication.java
@@ -1,13 +1,20 @@
 package ru.pxlhack.url_shortener;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UrlShortenerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UrlShortenerApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> System.out.println("This is URL shortener");
     }
 
 }
