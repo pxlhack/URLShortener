8de9178399386cbd5bcf3a5901b9c891fdffb251
pxlhack <nellsouhd@gmail.com>
2023-07-14 12:32:04 +0700

    feat: add model UrlMapping

---
+++ src/main/java/ru/pxlhack/url_shortener/models/UrlMapping.java
@@ -0,0 +1,30 @@
+package ru.pxlhack.url_shortener.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class UrlMapping {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "token")
+    private String token;
+
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @Column(name = "expired_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date expiredAt;
+}
